4d986db7a43419554321b09056cbbe58f07a6875
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2021-11-26 18:06:59 -0300

    segundo commit

---
+++ WebApplication2/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApplication2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuração e serviços de API Web
+
+            // Rotas de API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+        }
+    }
+}
---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        // GET api/values
+        public List<pessoa> Get()
+        {
+            List<pessoa> pessoas = new List<pessoa>();
+            using (var ctx = new BikeTeste())
+            {
+
+                var pf = new pessoa_fisica
+                {
+                    cpf = "2345678",
+                    name = "pessoa fisica"
+                };
+                var pj = new pessoa_juridica
+                {
+                    cnpj = "875421",
+                    name = "pessoa juridica"
+                };
+                ctx.pessoa.Add(pf);
+                ctx.pessoa.Add(pj);
+                ctx.SaveChanges();
+
+                pessoas = ctx.pessoa.ToList();
+
+            }
+            return pessoas;
+
+        }
+
+        //// GET api/values
+        //public List<personsSet> Get()
+        //{
+        //    List<personsSet> persons = new List<personsSet>();
+        //    using(var ctx = new BikeTeste())
+        //    {
+
+        //        var pf = new pessoa_fisica
+        //        {
+        //            cpf = "2345678",
+        //            name = "pessoa fisica"
+        //        };
+        //        var pj = new pessoa_juridica
+        //        {
+        //            cnpj = "875421",
+        //            name = "pessoa juridica"
+        //        };
+        //        ctx.pessoa.Add(pf);
+        //        ctx.pessoa.Add(pj);
+        //        ctx.SaveChanges();
+
+        //        #region old
+        //        var fisic = new personsSet_FisicPerson
+        //        {
+        //            cpf = "987654321",
+        //            name = "Person 1",
+        //            Id = 1
+        //        };
+        //        var jur = new personsSet_juridicpersons
+        //        {
+        //            cnpj = "12345678910121314",
+        //            name = "Person 2",
+        //            Id = 2
+        //        };
+        //        ctx.personsSet.Add(fisic);
+        //        ctx.personsSet.Add(jur);
+        //        ctx.SaveChanges();
+
+        //        persons = ctx.personsSet.ToList();
+
+        //        #endregion
+
+        //    }
+        //    return persons;
+
+        //}
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
